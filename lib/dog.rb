a58e882117008bd19471db102932f2a4e1becd41
jgumerove <josh.gumerove@gmail.com>
2020-12-10 00:38:51 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    
+  def initialize(name, breed= "Mutt")
+        @name = name
+        @breed = breed
+      end
+    
+      def breed=(name, breed)
+        @name = name
+        @breed = breed
+      end
+    
+      def breed
+        @name 
+        @breed
+      end
+     
+end
