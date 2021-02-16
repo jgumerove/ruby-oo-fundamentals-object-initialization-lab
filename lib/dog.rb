3c5534b35936e65c58418b036814cffbea91ddb0
jgumerove <josh.gumerove@gmail.com>
2021-02-16 18:22:52 -0500

    commit tree bba9421701cf2e555d893fa51fdab5a7620e00f4

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,9 @@
 class Dog
-    
-  def initialize(name,breed= "Mutt")
-        @name = name
-        @breed = breed
-      end
-    
-      def name=(name,breed)
-        @name = name
-        @breed = breed
-      end
-    
-      def name
-        @name 
-        @breed
-      end
-     
+
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
 end
+
